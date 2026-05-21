eeafcedb15ac44f5b89cb1b78380cfd99d644ce2
ahadith <ahadith>
2026-05-20 18:39:00 -0700

    Changing Elasticsearch component to pass along client IP

--- application/components/search/engines/ElasticConnection.php
+++ application/components/search/engines/ElasticConnection.php
@@ -2,6 +2,7 @@
 
 namespace app\components\search\engines;
 
+use Yii;
 use yii\base\BaseObject;
 
 class ElasticConnection extends BaseObject
@@ -10,20 +11,46 @@ class ElasticConnection extends BaseObject
     public $port;
     public $username;
     public $password;
-    private $context;
 
     public function init() {
         parent::init();
-        $opts = array(
-            'http' => array(
-                'header' => 'Authorization: Basic ' . base64_encode($this->username.':'.$this->password)
-            )
-        );
-        $this->context = stream_context_create($opts);
     }
 
     public function sendRequest($url)
     {
-        return file_get_contents("{$this->host}:{$this->port}{$url}", false, $this->context);
+        return file_get_contents("{$this->host}:{$this->port}{$url}", false, $this->createContext());
+    }
+
+    private function createContext()
+    {
+        return stream_context_create(array(
+            'http' => array(
+                'header' => implode("\r\n", $this->getHeaders()),
+            ),
+        ));
+    }
+
+    private function getHeaders()
+    {
+        $headers = array(
+            'Authorization: Basic ' . base64_encode($this->username.':'.$this->password),
+        );
+
+        $clientIp = $this->getClientIp();
+        if (!empty($clientIp)) {
+            $headers[] = 'X-Forwarded-For: ' . $clientIp;
+            $headers[] = 'X-Real-IP: ' . $clientIp;
+        }
+
+        return $headers;
+    }
+
+    private function getClientIp()
+    {
+        $request = Yii::$app->getRequest();
+
+        return $request->getHeaders()->get('CF-Connecting-IP')
+            ?: $request->getHeaders()->get('X-Forwarded-For')
+            ?: $request->getUserIP();
     }
 }
